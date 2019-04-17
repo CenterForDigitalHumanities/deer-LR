db47083f886c99e63f03c8a601ad2ac93864513a
Bryan H <thehabes@github.io>
2019-04-17 11:02:21 -0500

    Moving some things around

--- js/utils.js
+++ js/utils.js
@@ -3,14 +3,10 @@ import {DEER} from "../deer-lr-working/deer.js"
 //This will already contain 
 
 export default { 
-	myFirst:function(){
-		console.log("Hello world");
-		return "Hello World"
-	},
-
 	/**
-		Get the id out of an object.  @id and id fields need to be considered.  
-		return the id or blank if not found
+		*	Get the id out of an object.  @id and id fields need to be considered.  
+		*	@return the id or blank if not found
+		*	@param obj some JSON-LD object
 	 */
 	getObjID:function(obj){
 		let id = obj["@id"] ? obj["@id"] : obj.id ? obj.id : ""
