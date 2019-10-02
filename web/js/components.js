e7c0ece18286c7e4937e91cae013a9ec5a97a3c9
Patrick Cuba <cubap@slu.edu>
2019-10-02 16:24:52 -0500

    mapsapalooza

--- web/js/components.js
+++ web/js/components.js
@@ -4,9 +4,14 @@ class LrFooter extends HTMLElement {
         <img class="brand" src="https://www.slu.edu/marcom/tools-downloads/imgs/logo/left-aligned/slu_logoleftaligned_rgb.png">
         <img class="brand" src="https://blog.ongcdh.org/blog/wp-content/uploads/2018/05/logo-dark.png">
         <img class="brand" src="https://www.hluce.org/static/images/logo-hluce.png">`
-        this.classList.add('nav', 'nav-center', 'text-primary', 'is-fixed', 'is-full-width')
+        this.classList.add('nav', 'nav-center', 'text-primary', 'is-fixed', 'is-full-width', 'is-vertical-align')
         this.style.bottom = 0
         this.style.backgroundColor = "#FFF"
+        let spacer = document.createElement('div')
+        spacer.style.position = "relative"
+        spacer.style.display = "block"
+        spacer.style.height = this.offsetHeight * 1.2 + "px"
+        this.parentElement.insertBefore(spacer, this)
     }
 }
 customElements.define("lr-footer", LrFooter)
@@ -14,11 +19,11 @@ customElements.define("lr-footer", LrFooter)
 class LrNav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<div class="nav-left">
-        <a class="brand" href="./dashboard.html"><img src="http://religioninplace.org/blog/wp-content/uploads/2019/04/LRDA-Logo.jpg"></a>
+        <a class="brand" href="index.html"><img src="http://religioninplace.org/blog/wp-content/uploads/2019/04/LRDA-Logo.jpg"></a>
         <div class="tabs">
-            <a class="active">Dashboard</a>
-            <a>Locations</a>
-            <a>Stories</a>
+            <a class="active" href="dashboard.html">Dashboard</a>
+            <a href="places.html">Locations</a>
+            <a href="stories.html">Stories</a>
         </div>
     </div>
     <div class="nav-right">
