aba9493e9c772d1c4493c815b00077b4815d5d76
Patrick Cuba <cubap@slu.edu>
2019-10-29 14:08:04 -0500

    stripping down the global

--- web/js/deerInitializer.js
+++ web/js/deerInitializer.js
@@ -130,15 +130,15 @@ DEER.TEMPLATES.Event = function(obj, options = {}) {
         }
     }
     //
-DEER.URLS = {
-    BASE_ID: "http://devstore.rerum.io/v1",
-    CREATE: "create",
-    UPDATE: "update",
-    QUERY: "query",
-    OVERWRITE: "overwrite",
-    DELETE: "delete",
-    SINCE: "http://devstore.rerum.io/v1/since"
-}
+    // DEER.URLS = {
+    //     BASE_ID: "http://devstore.rerum.io/v1",
+    //     CREATE: "create",
+    //     UPDATE: "update",
+    //     QUERY: "query",
+    //     OVERWRITE: "overwrite",
+    //     DELETE: "delete",
+    //     SINCE: "http://devstore.rerum.io/v1/since"
+    // }
 
 // Render is probably needed by all items, but can be removed.
 // CDN at https://centerfordigitalhumanities.github.io/deer/releases/
