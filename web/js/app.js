6a8b9c9d306666f906e39348ee9b4d90a9d68658
Bryan Haberberger <bhaberbe@slu.edu>
2019-11-08 13:38:56 -0600

    Some re-staging for production was required.

--- web/js/app.js
+++ web/js/app.js
@@ -6,18 +6,18 @@
 
 const LR = {}
 LR.VERSION = "0.3.0"
-LR.APPAGENT = "http://devstore.rerum.io/v1/id/5afeebf3e4b0b0d588705d90"
+LR.APPAGENT = "http://store.rerum.io/v1/id/5da8c165d5de6ba6e2028474"
     //Make sure these behave like DEER.URLS, AKA when it is deployed to dev, use sandbox, not lived-religion-dev or the internal back end
 LR.URLS = {
     LOGIN: "login",
     LOGOUT: "logout",
-    BASE_ID: "http://devstore.rerum.io/v1",
-    DELETE: "http://tinydev.rerum.io/app/delete",
-    CREATE: "http://tinydev.rerum.io/app/create",
-    UPDATE: "http://tinydev.rerum.io/app/update",
-    OVERWRITE: "http://tinydev.rerum.io/app/overwrite",
-    QUERY: "http://tinydev.rerum.io/app/query",
-    SINCE: "http://devstore.rerum.io/v1/since"
+    BASE_ID: "http://store.rerum.io/v1",
+    DELETE: "delete",
+    CREATE: "create",
+    UPDATE: "update",
+    OVERWRITE: "overwrite",
+    QUERY: "query",
+    SINCE: "http://store.rerum.io/v1/since"
 }
 
 if (typeof(Storage) !== "undefined") {
