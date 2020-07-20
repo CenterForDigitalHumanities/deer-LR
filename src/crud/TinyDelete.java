c7c302c4719363a30e234f7ad8f8b11de258c3d9
Bryan H <thehabes@github.io>
2020-07-20 13:10:51 -0500

    Since deleting the experience will depend heavily on the primitive 'creator', it is best that only admins have the ability to delete collection items.  This has been implemented by a user session check in TinyDelete and a script check on the localStorage user for administrator privelage.

--- src/crud/TinyDelete.java
+++ src/crud/TinyDelete.java
@@ -5,6 +5,7 @@
  */
 package crud;
 
+import auth.Authorize;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -20,6 +21,8 @@ import javax.servlet.http.HttpServletResponse;
 import tokens.TinyTokenManager;
 import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpSession;
+import net.sf.json.JSONObject;
 
 /**
  *
@@ -46,63 +49,85 @@ public class TinyDelete extends HttpServlet {
         String requestString;
         StringBuilder sb = new StringBuilder();
         int codeOverwrite = 500;
-        boolean moveOn = true;
+        boolean moveOn = false;
         while ((line = bodyReader.readLine()) != null)
         {
           bodyString.append(line);
         }
         requestString = bodyString.toString();
-        //If it was JSON
         String pubTok = manager.getAccessToken();
         boolean expired = manager.checkTokenExpiry();
         if(expired){
             System.out.println("Lived Religion detected an expired token, auto getting and setting a new one...");
             pubTok = manager.generateNewAccessToken();
         }
-        //Point to rerum server v1
-        URL postUrl = new URL(Constant.RERUM_API_ADDR + "/delete.action");
-        HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
-        connection.setDoOutput(true);
-        connection.setDoInput(true);
-        connection.setRequestMethod("DELETE");
-        connection.setUseCaches(false);
-        connection.setInstanceFollowRedirects(true);
-        connection.setRequestProperty("Authorization", "Bearer "+pubTok);
-        connection.connect();
-        try{
-            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-            //Pass in the user provided JSON for the body of the rerumserver v1 request
-            byte[] toWrite = requestString.getBytes("UTF-8");
-            //out.writeBytes(requestJSON.toString());
-            out.write(toWrite);
-            out.flush();
-            out.close(); 
-            codeOverwrite = connection.getResponseCode();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
-            while ((line = reader.readLine()) != null){
-                //Gather rerum server v1 response
-                sb.append(line);
+        
+        /**
+         * Check that the session knows the user.
+         * Check if the known user is an administrator, only they can delete for the time being.
+         */
+        HttpSession sess = request.getSession();
+        if(sess.getAttribute("lr-user") != null){
+            JSONObject session_user = JSONObject.fromObject(sess.getAttribute("lr-user"));
+            if(session_user.getJSONObject("roles").getBoolean("administrator")){
+                moveOn = true;
             }
-            reader.close();
-            for (Map.Entry<String, List<String>> entries : connection.getHeaderFields().entrySet()) {
-                String values = "";
-                String removeBraks = entries.getValue().toString();
-                values = removeBraks.substring(1, removeBraks.length() -1);
-                if(null != entries.getKey() && !entries.getKey().equals("Transfer-Encoding")){
-                    response.setHeader(entries.getKey(), values);
+            else{
+                sb.append("You must be a Lived Religion administrator to delete at this time.");
+                codeOverwrite = HttpServletResponse.SC_UNAUTHORIZED;
+            }
+        }
+        else{
+            sb.append("Please login again.");
+            codeOverwrite = HttpServletResponse.SC_FORBIDDEN;
+        }
+        
+        if(moveOn){
+            //Point to rerum server v1
+            URL postUrl = new URL(Constant.RERUM_API_ADDR + "/delete.action");
+            HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("DELETE");
+            connection.setUseCaches(false);
+            connection.setInstanceFollowRedirects(true);
+            connection.setRequestProperty("Authorization", "Bearer "+pubTok);
+            connection.connect();
+            try{
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                //Pass in the user provided JSON for the body of the rerumserver v1 request
+                byte[] toWrite = requestString.getBytes("UTF-8");
+                //out.writeBytes(requestJSON.toString());
+                out.write(toWrite);
+                out.flush();
+                out.close(); 
+                codeOverwrite = connection.getResponseCode();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));
+                while ((line = reader.readLine()) != null){
+                    //Gather rerum server v1 response
+                    sb.append(line);
+                }
+                reader.close();
+                for (Map.Entry<String, List<String>> entries : connection.getHeaderFields().entrySet()) {
+                    String values = "";
+                    String removeBraks = entries.getValue().toString();
+                    values = removeBraks.substring(1, removeBraks.length() -1);
+                    if(null != entries.getKey() && !entries.getKey().equals("Transfer-Encoding")){
+                        response.setHeader(entries.getKey(), values);
+                    }
                 }
             }
+            catch(IOException ex){
+                //Need to get the response RERUM sent back.
+                BufferedReader error = new BufferedReader(new InputStreamReader(connection.getErrorStream(),"utf-8"));
+                String errorLine = "";
+                while ((errorLine = error.readLine()) != null){  
+                    sb.append(errorLine);
+                } 
+                error.close();
+            }
+            connection.disconnect();
         }
-        catch(IOException ex){
-            //Need to get the response RERUM sent back.
-            BufferedReader error = new BufferedReader(new InputStreamReader(connection.getErrorStream(),"utf-8"));
-            String errorLine = "";
-            while ((errorLine = error.readLine()) != null){  
-                sb.append(errorLine);
-            } 
-            error.close();
-        }
-        connection.disconnect();
         if(manager.getAPISetting().equals("true")){
             response.addHeader("Access-Control-Allow-Origin", "*"); //To use this as an API, it must contain CORS headers
         }
