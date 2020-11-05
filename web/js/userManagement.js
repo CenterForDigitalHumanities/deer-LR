f90cee8cf429530d5e6590cde85629bdbbf66d1a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2020-11-05 11:11:33 -0600

    context link fix

--- web/js/userManagement.js
+++ web/js/userManagement.js
@@ -104,7 +104,7 @@ UM.interaction.addUser = async function(){
     else{
         let agentObj = {
             "@type" : "Agent",
-            "@context" : "http://store.rerum.io/v1/contex.json",
+            "@context" : "http://store.rerum.io/v1/context.json",
             "mbox" : email,
             "label" : username,
             "name" : name
