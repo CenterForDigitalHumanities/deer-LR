2476fc281ba1414ef69c01f5665d42d630ffd0d1
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2021-09-27 11:18:34 -0500

    Area for media stuff in back end.  HTML page for basic testing.

---
+++ src/media/S3.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package media;
+
+/**
+ *
+ * @author bhaberbe
+ */
+public class S3 {
+    
+}
