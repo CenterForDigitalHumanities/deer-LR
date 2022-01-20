89ae3f4549c6bdcb2a664601afb128649c46d184
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-01-20 11:42:52 -0600

    RCG logo

--- web/js/components.js
+++ web/js/components.js
@@ -2,7 +2,7 @@ class LrFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<small>&copy;2020 Walter J. Ong, S.J. Center for Digital Humanities</small>
         <img class="brand" src="https://www.slu.edu/marcom/tools-downloads/imgs/logo/left-aligned/slu_logoleftaligned_rgb.png">
-        <img class="brand" src="https://blog.ongcdh.org/blog/wp-content/uploads/2018/05/logo-dark.png">
+        <img class="brand" src="https://centerfordigitalhumanities.github.io/media-assets/logos/rcg-logo.png">
         `
         this.classList.add('nav', 'nav-center', 'text-primary', 'is-fixed', 'is-full-width', 'is-vertical-align')
         this.style.bottom = 0
