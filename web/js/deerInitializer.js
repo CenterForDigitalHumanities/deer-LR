b3058d2ca1a9c47dc8e484bba0d30d1c6c5dde1f
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-02-09 09:02:25 -0600

    Staged for dev.  Small updates for media.

--- web/js/deerInitializer.js
+++ web/js/deerInitializer.js
@@ -351,15 +351,15 @@ let DEERprimitives = DEER.PRIMITIVES
 DEER.PRIMITIVES = [...DEERprimitives, ...LR_primitives]
 
 //Comment this out for dev-01 deploys
-DEER.URLS = {
-    BASE_ID: "http://store.rerum.io/v1",
-    CREATE: "create",
-    UPDATE: "update",
-    QUERY: "query",
-    OVERWRITE: "overwrite",
-    DELETE: "delete",
-    SINCE: "http://store.rerum.io/v1/since"
-}
+//DEER.URLS = {
+//    BASE_ID: "http://store.rerum.io/v1",
+//    CREATE: "create",
+//    UPDATE: "update",
+//    QUERY: "query",
+//    OVERWRITE: "overwrite",
+//    DELETE: "delete",
+//    SINCE: "http://store.rerum.io/v1/since"
+//}
 
 // Render is probably needed by all items, but can be removed.
 // CDN at https://deer.rerum.io/releases/
