16834be3de0e6c57e1f1ce7db1883095784907e5
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-03-31 13:31:48 -0500

    stage for prod

--- web/js/userManagement.js
+++ web/js/userManagement.js
@@ -10,7 +10,7 @@ UM.URLS = {
     SETSECRET: "setUserSecret",
     ADDUSER : "addUser",
     REMOVEUSER : "removeUser",
-    CREATE : "http://tinydev.rerum.io/app/create"
+    CREATE : "http://tiny.rerum.io/app/create"
 }
 
 /**
