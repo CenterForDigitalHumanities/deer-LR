875e77db2e98d85ff363757290f33e7d5554a177
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2023-05-01 11:33:24 -0500

    Stage for prod

--- src/crud/Constant.java
+++ src/crud/Constant.java
@@ -11,18 +11,18 @@ package crud;
  */
 public class Constant {
     //new dev
-    public static String RERUM_REGISTRATION_URL = "http://vlcdhp02:3001/v1/";
-    public static String RERUM_API_ADDR = "http://vlcdhp02:3001/v1/api";
-    public static String RERUM_ACCESS_TOKEN_URL = "http://vlcdhp02:3001/v1/api/accessToken.action";
-    public static String RERUM_REFRESH_TOKEN_URL = "http://vlcdhp02:3001/v1/api/refreshToken.action";
-    public static String RERUM_AGENT = "http://devstore.rerum.io/v1/id/5afeebf3e4b0b0d588705d90";
+//    public static String RERUM_REGISTRATION_URL = "http://vlcdhp02:3001/v1/";
+//    public static String RERUM_API_ADDR = "http://vlcdhp02:3001/v1/api";
+//    public static String RERUM_ACCESS_TOKEN_URL = "http://vlcdhp02:3001/v1/api/accessToken.action";
+//    public static String RERUM_REFRESH_TOKEN_URL = "http://vlcdhp02:3001/v1/api/refreshToken.action";
+//    public static String RERUM_AGENT = "http://devstore.rerum.io/v1/id/5afeebf3e4b0b0d588705d90";
     
     //new prod
-//    public static String RERUM_REGISTRATION_URL = "http://vlcdhprdp02:3001/v1/";
-//    public static String RERUM_API_ADDR = "http://vlcdhprdp02:3001/v1/api";
-//    public static String RERUM_ACCESS_TOKEN_URL = "http://vlcdhprdp02:3001/v1/api/accessToken.action";
-//    public static String RERUM_REFRESH_TOKEN_URL = "http://vlcdhprdp02:3001/v1/api/refreshToken.action";
-//    public static String RERUM_AGENT = "http://store.rerum.io/v1/id/5da8c165d5de6ba6e2028474";
+    public static String RERUM_REGISTRATION_URL = "http://vlcdhprdp02:3001/v1/";
+    public static String RERUM_API_ADDR = "http://vlcdhprdp02:3001/v1/api";
+    public static String RERUM_ACCESS_TOKEN_URL = "http://vlcdhprdp02:3001/v1/api/accessToken.action";
+    public static String RERUM_REFRESH_TOKEN_URL = "http://vlcdhprdp02:3001/v1/api/refreshToken.action";
+    public static String RERUM_AGENT = "http://store.rerum.io/v1/id/5da8c165d5de6ba6e2028474";
     
     //https://stackoverflow.com/questions/2395737/java-relative-path-of-a-file-in-a-java-web-application
     public static String PROPERTIES_FILE_NAME = "lr.properties";
